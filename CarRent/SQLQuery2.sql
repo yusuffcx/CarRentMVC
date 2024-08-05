183042168976a9162cd8d4e6515c882ba8f2452d
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-08-05 10:41:52 +0300

    edited home controller

--- CarRent/SQLQuery2.sql
+++ CarRent/SQLQuery2.sql
@@ -1,10 +1,10 @@
-﻿/*insert into Vehicles(CompanyID,Brand,Model,Color,Year,DailyRate,LicensePlate,AvailableFrom,
+﻿insert into Vehicles(CompanyID,Brand,Model,Color,Year,DailyRate,LicensePlate,AvailableFrom,
 AvailableTo,FuelType,TransmissionType,MileAge) 
-values (1,'Renault','Clio','Beyaz',2023,650,'34 KM 225',getdate(),DATEADD(dd,35,GETDATE()),'Dizel','Yarı Otomatik',2456);
-*/
+values (1,'Fiat','Fiorino','Derin Mavi',2024,850,'34 FT 1475',DATEADD(dd,8,GETDATE()),DATEADD(dd,60,GETDATE()),'Dizel','Manuel',1745);
 
-/*
-insert into VehicleImages(VehicleID,ImagePath) values (scope_identity(),'\images\clio2023.jpg');
+
+
+insert into VehicleImages(VehicleID,ImagePath) values (scope_identity(),'\images\fiorino2024.jpg');
 select * from  VehicleImages;
 select * from  Vehicles;
 
