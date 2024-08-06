a7b2c0e18b06e0db5b178b7318f494cb3b51566e
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-08-06 14:19:24 +0300

    adding seller controller for seller panel

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,10 +1,3 @@
-﻿/*insert into Vehicles(CompanyID,Brand,Model,Color,Year,DailyRate,LicensePlate,AvailableFrom,
-AvailableTo,FuelType,TransmissionType,MileAge) 
-values (1,'Renault','Clio','Beyaz',2023,650,'34 KM 225',getdate(),DATEADD(dd,35,GETDATE()),'Dizel','Yarı Otomatik',2456);
-*/
+﻿insert into Users(Username,Password,Email,RoleID,PhoneNumber) values ('brent','1667','brent@outlook.com',3,'02124442112');
 
-/*
-insert into VehicleImages(VehicleID,ImagePath) values (scope_identity(),'\images\clio2023.jpg');
-select * from  VehicleImages;
-select * from  Vehicles;
-*/
+select * from Users;
